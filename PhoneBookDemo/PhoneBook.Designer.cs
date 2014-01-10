674635df733b1ced300b960c7bfb8e8b68da8629
megibyan <megibyan@gmail.com>
2014-01-10 02:48:38 -0800

    Grid

---
+++ PhoneBookDemo/PhoneBook.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace PhoneBookDemo
+{
+    partial class PhoneBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.DataVisualisationGrid = new System.Windows.Forms.DataGridView();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PhoneNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.writeToAFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.readFromAFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataVisualisationGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem1,
+            this.openToolStripMenuItem1,
+            this.deleteToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.writeToAFileToolStripMenuItem,
+            this.readFromAFileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(493, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip";
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(43, 20);
+            this.saveToolStripMenuItem1.Text = "Save";
+            this.saveToolStripMenuItem1.Click += new System.EventHandler(this.saveToolStripMenuItem1_Click);
+            // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(48, 20);
+            this.openToolStripMenuItem1.Text = "Open";
+            this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click);
+            // 
+            // DataVisualisationGrid
+            // 
+            this.DataVisualisationGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataVisualisationGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FirstName,
+            this.LastName,
+            this.PhoneNumber});
+            this.DataVisualisationGrid.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.DataVisualisationGrid.Location = new System.Drawing.Point(0, 27);
+            this.DataVisualisationGrid.MultiSelect = false;
+            this.DataVisualisationGrid.Name = "DataVisualisationGrid";
+            this.DataVisualisationGrid.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.DataVisualisationGrid.Size = new System.Drawing.Size(493, 288);
+            this.DataVisualisationGrid.TabIndex = 1;
+            // 
+            // FirstName
+            // 
+            this.FirstName.HeaderText = "First Name";
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Width = 150;
+            // 
+            // LastName
+            // 
+            this.LastName.HeaderText = "Last Name";
+            this.LastName.Name = "LastName";
+            this.LastName.Width = 150;
+            // 
+            // PhoneNumber
+            // 
+            this.PhoneNumber.HeaderText = "Phone Number";
+            this.PhoneNumber.Name = "PhoneNumber";
+            this.PhoneNumber.Width = 150;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click_1);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // writeToAFileToolStripMenuItem
+            // 
+            this.writeToAFileToolStripMenuItem.Name = "writeToAFileToolStripMenuItem";
+            this.writeToAFileToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.writeToAFileToolStripMenuItem.Text = "Write to a file";
+            this.writeToAFileToolStripMenuItem.Click += new System.EventHandler(this.writeToAFileToolStripMenuItem_Click);
+            // 
+            // readFromAFileToolStripMenuItem
+            // 
+            this.readFromAFileToolStripMenuItem.Name = "readFromAFileToolStripMenuItem";
+            this.readFromAFileToolStripMenuItem.Size = new System.Drawing.Size(102, 20);
+            this.readFromAFileToolStripMenuItem.Text = "Read from a file";
+            this.readFromAFileToolStripMenuItem.Click += new System.EventHandler(this.readFromAFileToolStripMenuItem_Click);
+            // 
+            // PhoneBook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 314);
+            this.Controls.Add(this.DataVisualisationGrid);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "PhoneBook";
+            this.Text = "Phonebook";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataVisualisationGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.DataGridView DataVisualisationGrid;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PhoneNumber;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem writeToAFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem readFromAFileToolStripMenuItem;
+    }
+}
+
---
+++ PhoneBookDemo/PhoneBook.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace PhoneBookDemo
+{
+    public partial class PhoneBook : Form
+    {
+        public PhoneBook(){
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e){
+        }
+
+        [Serializable]
+
+        public class Data{
+            public string FirstName;
+            public string LastName;
+            public string PhoneNumber;
+        }
+
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e){
+            DataVisualisationGrid.EndEdit();
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream output = new FileStream(saveFileDialog1.FileName, FileMode.OpenOrCreate, FileAccess.Write);
+                //saveFileDialog1.DefaultExt = "txt";
+                //saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+                int n = DataVisualisationGrid.RowCount;
+                Data[] Person = new Data[n - 1];
+                for (int i = 0; i < n - 1; i++)
+                {
+                    Person[i] = new Data();
+                    Person[i].FirstName = DataVisualisationGrid[0, i].Value.ToString();
+                    Person[i].LastName = DataVisualisationGrid[1, i].Value.ToString();
+                    Person[i].PhoneNumber = DataVisualisationGrid[2, i].Value.ToString();
+                }
+
+                formatter.Serialize(output, Person);
+
+                output.Close();
+            }
+        }
+
+        private void openToolStripMenuItem1_Click(object sender, EventArgs e){
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                BinaryFormatter reader = new BinaryFormatter();
+                FileStream input = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read);
+
+                Data[] Person = (Data[])reader.Deserialize(input);
+                DataVisualisationGrid.Rows.Clear();
+                for (int i = 0; i < Person.Length; i++)
+                {
+                    DataVisualisationGrid.Rows.Add();
+                    DataVisualisationGrid[0, i].Value = Person[i].FirstName;
+                    DataVisualisationGrid[1, i].Value = Person[i].LastName;
+                    DataVisualisationGrid[2, i].Value = Person[i].PhoneNumber;
+                }
+            }
+        }
+
+        private void deleteToolStripMenuItem_Click_1(object sender, EventArgs e){
+            foreach (DataGridViewRow dr in DataVisualisationGrid.SelectedRows)
+            {
+
+                if (dr.Cells[0].Value.ToString() != null)
+                    DataVisualisationGrid.Rows.Remove(dr);
+            }
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e){
+            DataVisualisationGrid.Rows.Clear();
+        }
+
+        private void writeToAFileToolStripMenuItem_Click(object sender, EventArgs e){
+            DataVisualisationGrid.EndEdit();
+
+            SaveFileDialog saveFileDialog2 = new SaveFileDialog();
+            saveFileDialog2.RestoreDirectory = true;
+
+            if (saveFileDialog2.ShowDialog() == DialogResult.OK){
+                FileStream output = new FileStream(saveFileDialog2.FileName, FileMode.OpenOrCreate, FileAccess.Write);
+                //saveFileDialog1.DefaultExt = "txt";
+                //saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                output.Close();
+                System.IO.File.WriteAllText(saveFileDialog2.FileName, string.Empty);
+                int n = DataVisualisationGrid.RowCount;
+                Data[] Person = new Data[n - 1];
+                for (int i = 0; i < n - 1; i++)
+                {
+                    Person[i] = new Data();
+                    Person[i].FirstName = DataVisualisationGrid[0, i].Value.ToString();
+                    Person[i].LastName = DataVisualisationGrid[1, i].Value.ToString();
+                    Person[i].PhoneNumber = DataVisualisationGrid[2, i].Value.ToString();
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(saveFileDialog2.FileName, true))
+                        file.WriteLine(Person[i].FirstName + '/' + Person[i].LastName + '/' + Person[i].PhoneNumber);
+                }
+
+            }
+        }
+
+        private void readFromAFileToolStripMenuItem_Click(object sender, EventArgs e){
+
+        }
+    }
+}
+
